4b5ea68a998c16702c65362398f3d0fdc95e15f4
miche <miche@DESKTOP-EEADCHD>
2025-02-20 19:32:04 -0600

    commit 12+1 añadiendo controladores/crud

---
+++ L01_2022AE650_2023CA651/Controllers/comentariosController.cs
@@ -0,0 +1,26 @@
+﻿using L01_2022AE650_2023CA651.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class comentariosController : ControllerBase
+{
+    private readonly comentariosContext _comentariosContext;
+    public comentariosController(comentariosContext comentariosContext)
+    {
+        _comentariosContext = comentariosContext;
+    }
+
+    [HttpGet]
+    [Route("GetAll")]
+
+    public IActionResult Get()
+    {
+        List<comentarios> listadoComentarios = (from e in _comentariosContext.comentarios select e).ToList();
+        if (listadoComentarios.Count() == 0)
+        {
+            return NotFound();
+        }
+        return Ok(listadoComentarios);
+    }
+
+}
+}
---
+++ L01_2022AE650_2023CA651/Controllers/usuariosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_2022AE650_2023CA651.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_2022AE650_2023CA651.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        private readonly usuariosContext _usuariosContext;
+        public usuariosController(usuariosContext usuariosContext)
+        {
+           _usuariosContext = usuariosContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<usuarios> listadoUsuarios = (from e in _usuariosContext.usuarios select e).ToList();
+            if (listadoUsuarios.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoUsuarios);
+        }
+
+    }
+}
