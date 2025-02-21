54038f340a2da0d9c344c17d100eb28ec77bca5a
miche <miche@DESKTOP-EEADCHD>
2025-02-20 18:51:46 -0600

    commit 8 publicaciones context

---
+++ L01_2022AE650_2023CA651/Models/publicacionesContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01_2022AE650_2023CA651.Models
+{
+    public class publicacionesContext : DbContext
+    {
+        public publicacionesContext(DbContextOptions<publicacionesContext> options) : base(options)
+        {
+        }
+
+    }
+}
