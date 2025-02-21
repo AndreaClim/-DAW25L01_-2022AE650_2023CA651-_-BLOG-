e6a24268fd65ab30a982e9799cdc772f2c583018
miche <miche@DESKTOP-EEADCHD>
2025-02-20 19:49:36 -0600

    COMMIT 16 CORRECCIONES EN COMENTARIOS CONTROLADORES

--- L01_2022AE650_2023CA651/Controllers/comentariosController.cs
+++ L01_2022AE650_2023CA651/Controllers/comentariosController.cs
@@ -1,25 +1,31 @@
-﻿using L01_2022AE650_2023CA651.Models;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using L01_2022AE650_2023CA651.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class comentariosController : ControllerBase
+namespace L01_2022AE650_2023CA651.Controllers
 {
-    private readonly comentariosContext _comentariosContext;
-    public comentariosController(comentariosContext comentariosContext)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class comentariosController : ControllerBase
     {
-        _comentariosContext = comentariosContext;
-    }
-
-    [HttpGet]
-    [Route("GetAll")]
-
-    public IActionResult Get()
-    {
-        List<comentarios> listadoComentarios = (from e in _comentariosContext.comentarios select e).ToList();
-        if (listadoComentarios.Count() == 0)
+        private readonly comentariosContext _comentariosContext;
+        public comentariosController(comentariosContext comentariosContext)
         {
-            return NotFound();
+            _comentariosContext = comentariosContext;
         }
-        return Ok(listadoComentarios);
-    }
 
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<comentarios> listadoComentarios = (from e in _comentariosContext.comentarios select e).ToList();
+            if (listadoComentarios.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoComentarios);
+        }
+    }
 }
